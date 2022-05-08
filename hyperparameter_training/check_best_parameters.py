9816ffe94395853d92218d0ce3c77c90b88e8483
sudeepsalgia <suda96salgia@gmail.com>
2022-05-07 20:16:40 -0400

    Adding synthetic results

--- hyperparameter_training/check_best_parameters.py
+++ hyperparameter_training/check_best_parameters.py
@@ -3,16 +3,16 @@ import itertools
 import pickle
 
 eta_vec = [1e-3, 1e-2, 1e-1]
-# lambda_vec = [0.05, 0.1, 0.5]
-lambda_vec = [0.1, 0.5, 1]
+lambda_vec = [0.05, 0.1, 0.5]
+# lambda_vec = [0.1, 0.5, 1]
 
 
-reward_func = 'inner_product_squared'
-algo = 'NeuralUCB'
+reward_func = 'xAAx'
+algo = 'SupNNUCB'
 
 for eta, _lambda in itertools.product(eta_vec, lambda_vec):
 
-	filename = './synthetic_data/Hyperparamter_' + reward_func + '_' + algo + '_' + str(int(-np.log10(eta))) + '_' + str(int(10*_lambda)) + '.pkl'
+	filename = './synthetic_data/Hyperparamter_' + reward_func + '_' + algo + '_' + str(int(-np.log10(eta))) + '_' + str(int(100*_lambda)) + '.pkl'
 	with open(filename, 'rb') as f:
 		saved_tuple = pickle.load(f)
 	f.close()
---
+++ save_neuralts.py
@@ -0,0 +1,100 @@
+import numpy as np
+import itertools
+import pickle
+from bandit import *
+from neuralucb import *
+from supnnucb import *
+from newalg import *
+from batchedneuralucb import *
+from neuralts import *
+
+# Check all settings before running
+
+# Changes on following lines:
+# Change algo name on line 63
+# Change function on line 42
+# Change function name on line 59
+# Change B on line 33
+
+time_horizon = 1000
+n_arms = 4
+n_features = 10
+noise_std = 0.1
+nu = 0.1
+n_sim = 15
+bandit_seed = 42
+nn_seeds = (np.random.random(n_sim)*1000).astype('int')
+delta = 0.1
+eta = 0.01
+_lambda = 0.5
+
+# Neural Network parameters
+hidden_size = 20
+epochs = 200
+train_every = 1
+use_cuda = False
+B = 8
+s = 1
+
+fns = ['xAAx', 'inner_product_squared', 'cosine']
+
+for fn in fns:
+	if fn == 'xAAx':
+		np.random.seed(bandit_seed*2)
+		A = np.random.normal(scale=0.5, size=(n_features,n_features))
+		reward_func = lambda x: np.linalg.norm(np.dot(A, x), ord=2)
+		a = A
+	elif fn == 'inner_product_squared':
+		np.random.seed(bandit_seed*2)
+		a = np.random.randn(n_features)
+		a /= np.linalg.norm(a, ord=2)
+		reward_func = lambda x: 4*np.dot(a, x)**2
+	else:
+		np.random.seed(bandit_seed*2)
+		a = np.random.randn(n_features)
+		a /= np.linalg.norm(a, ord=2)
+		reward_func = lambda x: 4*np.sin(np.dot(a, x))**2
+
+	bandit = ContextualBandit(time_horizon, n_arms, n_features, reward_func, noise_std=noise_std, seed=bandit_seed)
+
+	settings = {'T': time_horizon,
+				'n_arms': n_arms,
+				'n_features': n_features,
+				'noise std dev': noise_std,
+				'nu': nu,
+				'n_sim': n_sim,
+				'bandit seed': bandit_seed,
+				'nn seeds': nn_seeds,
+				'hidden_size': hidden_size,
+				'epochs': epochs,
+				'train_every': train_every,
+				'a_vec': a,
+				'reward_func': fn,
+				'B': B,
+				'activation function': 'ReLU ' + str(s),
+				'delta': delta,
+				'algo': 'NeuralTS',
+				'eta': eta,
+				'lambda': _lambda }                                        
+
+
+	regrets = np.empty((n_sim, time_horizon))
+	time_taken = np.empty(n_sim)
+
+
+	for n in range(n_sim):
+		bandit.reset_rewards()
+		model = NeuralTS(bandit, hidden_size=hidden_size, _lambda=_lambda, delta=delta, nu=nu, training_window=time_horizon,
+						  eta=eta, B=B, epochs=epochs, train_every=train_every, use_cuda=use_cuda, activation_param=s, model_seed=nn_seeds[n])
+
+		model.run()
+		regrets[n] = np.cumsum(model.regret)
+		time_taken[n] = model.time_elapsed
+
+	save_tuple = (settings, regrets, time_taken)
+	filename = './' + settings['algo'] + '_' + settings['reward_func'] + '.pkl'
+	with open(filename, 'wb') as f:
+		pickle.dump(save_tuple, f)
+	f.close()
+
+
